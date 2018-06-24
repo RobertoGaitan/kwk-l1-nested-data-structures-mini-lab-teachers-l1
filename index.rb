78a57e2e39bcb41752b6efb1817f98786bf895b0
Roberto Gaitan <rgaitan.it@gmail.com>
2018-06-24 02:00:48 +0000

    Done.

--- index.rb
+++ index.rb
@@ -6,28 +6,45 @@ ingredients = [
     ["mayo", "mustard", "tabasco"]
 ]
 
-def sandwich_request(ingredients, order) 
-  # your code here!
+def sandwich_request(ingredients, order)
+  all_found = Array.new(order.length)
+  counter = 0
+  order.each do |item|
+    all_found[counter] = "F"
+    ingredients.each do |ingredient_row|
+      ingredient_row.each do |ingredient|
+        if item == ingredient
+          all_found[counter] = "T"
+        end
+      end
+    end
+    counter += 1
+  end
+  if all_found.include? "F"
+    return FALSE
+  else
+    return true
+  end
 end
 
 # example city_info hash
 cities = {
-  :new_york => { 
+  :new_york => {
     :mayor => "Bill DeBlasio",
     :population => 8337000,
     :website => "http://www.nyc.gov"
   },
-  :los_angeles => { 
+  :los_angeles => {
     :mayor => "Eric Garcetti",
     :population => 3884307,
     :website => "http://www.lacity.org"
   },
-  :miami => { 
+  :miami => {
     :mayor => "Tomás Regalado",
     :population => 419777,
     :website => "http://www.miamigov.com"
   },
-  :chicago => { 
+  :chicago => {
     :mayor => "Rahm Emanuel",
     :population => 2695598,
     :website => "http://www.cityofchicago.org/"
@@ -35,5 +52,10 @@ cities = {
 }
 
 def city_populations(cities)
-  # your code here!
+  totpop = 0
+   cities.each do |city, data| 
+    totpop += data[:population]
+ #   puts "#{totpop}"
+  end
+  return totpop
 end
